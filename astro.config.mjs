116202677f87ad7f2a3a9eb15a14d86edc9297c7
euaell <euaelmeko@gmail.com>
2026-02-07 17:36:17 +0300

    fix: add sitemap integration and update dependencies for improved functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   output: "server",
   adapter: node({
     mode: "standalone",
   }),
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   vite: {
     build: {
       outDir: "dist",
